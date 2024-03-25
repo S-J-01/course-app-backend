804bdfd7c027416ea7918bfa614641cd1db22cac
Shreyas Jaltare <shreyasjaltare@gmail.com>
2024-03-25 23:21:05 +0530

    Added user routes

--- 02-course-app-easy/index.js
+++ 02-course-app-easy/index.js
@@ -61,6 +61,27 @@ app.post('/admin/courses', (req, res) => {
 
 app.put('/admin/courses/:courseId', (req, res) => {
   // logic to edit a course
+  var username = req.headers.username;
+  var password = req.headers.password;
+
+  var adminExists = ADMINS.find((obj)=>obj.username===username && obj.password===password);
+  if(adminExists){
+    var courseId = req.params.courseId;
+    var courseToUpdate = COURSES.find((obj)=>obj.courseId===courseId);
+    if(courseToUpdate){
+    courseToUpdate.title=req.body.title;
+    courseToUpdate.description=req.body.description;
+    courseToUpdate.price = req.body.price;
+    courseToUpdate.imageLink=req.body.imageLink;
+    courseToUpdate.published=req.body.published;
+
+    res.status(200).json({message:'course updated successfully'});
+    }else{
+      res.status(400).json({message:'wrong course id'});
+    }
+  }else{
+    res.status(400).json({message:'wrong username or password'});
+  }
 });
 
 app.get('/admin/courses', (req, res) => {
@@ -81,22 +102,72 @@ app.get('/admin/courses', (req, res) => {
 // User routes
 app.post('/users/signup', (req, res) => {
   // logic to sign up user
+  var newUser = {};
+  newUser.username=req.body.username;
+  newUser.password=req.body.password;
+  newUser.purchasedCourses = [];
+  USERS.push(newUser);
+
+  res.status(200).json({message:'User created successfully'});
 });
 
 app.post('/users/login', (req, res) => {
   // logic to log in user
+  var username= req.headers.username;
+  var password = req.headers.password;
+
+  var userExists= USERS.find((obj)=>obj.username===username && obj.password===password);
+  if(userExists){
+    res.status(200).json({message:'logged in successfully'});
+  }else{
+    res.status(400).json({message:'wrong username or passsword'});
+  }
 });
 
 app.get('/users/courses', (req, res) => {
   // logic to list all courses
+  var username= req.headers.username;
+  var password = req.headers.passsword;
+
+  var userExists= USERS.find((obj)=>obj.username===username&&obj.password===password);
+  if(userExists){
+    res.status(200).json({courses:COURSES});
+  }else{
+    res.status(400).json({message:'wrong username or password'});
+  }
 });
 
 app.post('/users/courses/:courseId', (req, res) => {
   // logic to purchase a course
+  var username= req.headers.username;
+  var password = req.headers.password;
+  var courseIdToBePurchased = req.params.courseId;
+  var userExists= USERS.find((obj)=>obj.username===username&&obj.password===password);
+  if(userExists){
+    var courseToBePurchased = COURSES.find((obj)=>obj.courseId===courseIdToBePurchased);
+    if(courseToBePurchased){
+      userExists.purchasedCourses.push(courseToBePurchased);
+      res.status(200).json({message:'course purchased successfully'});
+    }else{
+      res.status(400).json({message:'wrong course ID'});
+    }
+  }else{
+    res.status(400).json({message:'wrong username or password'});
+  }
 });
 
 app.get('/users/purchasedCourses', (req, res) => {
   // logic to view purchased courses
+  var username= req.headers.username;
+  var password = req.headers.passsword;
+
+  var userExists= USERS.find((obj)=>obj.username===username&&obj.password===password);
+
+  if(userExists){
+    res.status(200).json({purchasedCourses:userExists.purchasedCourses});
+  }else{
+    res.status(400).json({message:'wrong username or password'});
+  }
 });
 
 app.listen(3000, () => {
